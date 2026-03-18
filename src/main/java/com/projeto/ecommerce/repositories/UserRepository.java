6ca035636558800ad7794a0a75466578c2ed749f
andrezon04 <diasa0421@gmail.com>
2026-03-18 11:03:15 -0300

    feat: create the struture of code and classes

---
+++ src/main/java/com/projeto/ecommerce/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.ecommerce.repositories;
+
+import com.projeto.ecommerce.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+}
